2084ca75b130711a71a287ca13ff4856e8dbd42c
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2019-11-06 20:45:01 -0300

    Create Consulta.sql

---
+++ Consulta.sql
@@ -0,0 +1,8 @@
+INSERT INTO estado(estado_codigo, estado_nome) //Seleciona a tabela e os atributos onde irá inserir dados
+VALUES (16, 'São Paulo'); //Valores a serem inseridos nos atributos
+
+INSERT INTO cliente(cliente_nome)  //Seleciona a tabela e os atributos onde irá inserir dados
+VALUES ('Gabriel'); //Valores a serem inseridos nos atributos
+
+INSERT INTO produto(produto_codigo, produto_nome, produto_preco)  //Seleciona a tabela e os atributos onde irá inserir dados
+VALUES (1, 'Cama', 500); //Valores a serem inseridos nos atributos
